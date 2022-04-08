1c58ac57a407fadf35d16359c2a798402b0833b5
Andy <andymcdonald1985@gmail.com>
2022-04-08 16:46:10 +0100

    Added part 2

--- app_p2.py
+++ app_p2.py
@@ -2,20 +2,17 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
+st.set_page_config(layout="wide")
 
-upload_file = st.file_uploader('Upload a file containing earthquake data')
-
-if upload_file is not None:
-    df = pd.read_csv(upload_file)
-
+def data_summary():
     st.header('Statistics of Dataframe')
     st.write(df.describe())
 
+def data_header():
     st.header('Header of Dataframe')
     st.write(df.head())
 
+def displayplot():
     st.header('Plot of Data')
     
     fig, ax = plt.subplots(1,1)
@@ -24,3 +21,24 @@ if upload_file is not None:
     ax.set_ylabel('Magnitude')
     
     st.pyplot(fig)
+
+st.title('Earthquake Data Explorer')
+st.text('This is a web app to allow exploration of Earthquake Data')
+
+st.sidebar.title('Sidebar')
+upload_file = st.sidebar.file_uploader('Upload a file containing earthquake data')
+#Sidebar navigation
+st.sidebar.title('Navigation')
+options = st.sidebar.radio('Select what you want to display:', ['Data Summary', 'Data Header', 'Scatter Plot'])
+
+
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+
+if options == 'Data Summary':
+    data_summary()
+elif options == 'Data Header':
+    data_header()
+elif options == 'Scatter Plot':
+    displayplot()
+
